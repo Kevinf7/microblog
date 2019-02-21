f16c8cf619e8ff6483cd6dc367a6b44e22e73fab
Kevin Foong <kevin_foong@yahoo.com>
2019-02-21 20:31:09 +1100

    remove passwords

--- config.py
+++ config.py
@@ -7,9 +7,7 @@ class Config(object):
     DEBUG=True
 
     #Flask-SQLAlchemy settings
-    #SQLALCHEMY_DATABASE_URI='mysql+pymysql://u517628157_kf:kSdxKhRTm1Nf@sql171.main-hosting.eu/u517628157_tt'
-    #SQLALCHEMY_DATABASE_URI='mysql+pymysql://kevinf7:Sydn3y3030@kevinf7.mysql.pythonanywhere-services.com/kevinf7$top_trumps'
-    SQLALCHEMY_DATABASE_URI='mysql+pymysql://kevin:Sydn3y30@localhost:3306/blog'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_POOL_RECYCLE = 299
     SQLALCHEMY_TRACK_MODIFICATIONS=False
     POSTS_PER_PAGE = 5
